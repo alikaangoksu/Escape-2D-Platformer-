e97acf6365a9577f9643fd58e902c8fc09131269
Ali Kaan Goksu <47613498+alikaangoksu@users.noreply.github.com>
2019-05-25 22:12:41 +0300

    Add files via upload

--- Scripts/CoinCollect.cs
+++ Scripts/CoinCollect.cs
@@ -5,11 +5,24 @@ public class CoinCollect : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickUp;
     [SerializeField] int coinAdd = 1;
+    public GameObject[] respawns;
+    public bugfix c;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        FindObjectOfType<GameSession>().coinScore(coinAdd);
-        AudioSource.PlayClipAtPoint(coinPickUp, Camera.main.transform.position);
-        Destroy(gameObject);
+        c = FindObjectOfType<bugfix>();
+        if (!c.changed)
+        {
+            GameObject.FindGameObjectsWithTag("JumpTrigger");
+            {
+                FindObjectOfType<GameSession>().coinScore(coinAdd);
+                AudioSource.PlayClipAtPoint(coinPickUp, Camera.main.transform.position);
+
+                Destroy(gameObject);
+                
+                c.changed = true;
+            }
+        }
+
     }
 }
