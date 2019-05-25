132022bc9c6bd9b2d60812d8b6032efa5ab34f63
BerkeSaka <48902691+BerkeSaka@users.noreply.github.com>
2019-05-26 01:43:19 +0300

    Add files via upload

---
+++ Scripts/TimeManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeManager : MonoBehaviour
+{
+    
+    int i = 0;
+    // [SerializeField] float  startingTime=15;
+    IList<float> startingTime = new List<float>() { 15, 25,30, 45, 50, 100,25,30,45,50,25,30,45,50 };
+    [SerializeField] float countingTime;
+
+    private Text timeText;
+
+
+    void Start()
+    {
+
+        countingTime = startingTime[i];
+        timeText = GetComponent<Text>();
+
+
+    }
+    public void timeReset()
+    {
+
+        i++;
+        //startingTime += 15;
+        countingTime = startingTime[i];
+
+
+    }
+    public void setTime()
+    {
+        countingTime = 15;
+    }
+   
+    void Update()
+    {
+        countingTime -= Time.deltaTime;
+     
+        if (countingTime<=0)
+        {
+            countingTime = countingTime + 0.5f;
+             FindObjectOfType<GameSession>().PlayerDeath();
+        }
+        timeText.text = "" + Mathf.Round (countingTime);
+    }
+}
---
+++ Scripts/pauseScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class pauseScript : MonoBehaviour
+{
+    GameObject[] pauseObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseObjects = GameObject.FindGameObjectsWithTag("ShowOnPause");
+        hidePaused();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1) { 
+
+                Time.timeScale = 0;
+                showPaused();
+
+            }
+            else { 
+                Time.timeScale = 1;
+                hidePaused();
+            }
+        }
+    }
+    public void pauseGame()
+    {
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            showPaused();
+        }
+        else if (Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            hidePaused();
+        }
+    }
+    public void showPaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    //hides objects with ShowOnPause tag
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+    public void ReturnMenu()
+    {
+        hidePaused();
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+        FindObjectOfType<GameSession>().coinReset();
+        FindObjectOfType<TimeManager>().setTime();
+    }
+}
