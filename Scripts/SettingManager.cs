e7d149f6c3f4044cd91d843b8b9be97a26720b0c
Kazim Ataol Ozuseven <kazimataol96@gmail.com>
2019-05-25 22:19:27 +0300

    Add files via upload

---
+++ Scripts/SettingManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+public class SettingManager : MonoBehaviour
+{
+    public Toggle fullscreenToggle;
+    public Dropdown resolutionDropdown;
+    public Dropdown textureQualityDropdown;
+    public Slider soundVolumeSlider;
+    public Button applyButton;
+
+    public AudioSource soundSource;
+    public Resolution[] resolutions;
+    public GameSettings gameSettings;
+
+     void OnEnable()
+    {
+
+        gameSettings = new GameSettings();
+
+        fullscreenToggle.onValueChanged.AddListener(delegate { OnFullscreenToggle(); });
+        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
+        textureQualityDropdown.onValueChanged.AddListener(delegate { OnTextureQualityChange(); });
+        soundVolumeSlider.onValueChanged.AddListener(delegate { OnSoundVolumeChange(); });
+        applyButton.onClick.AddListener(delegate { OnApplyButtonClick(); });
+
+        resolutions = Screen.resolutions;
+        foreach(Resolution resolution in resolutions)
+        {
+            resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+        }
+        LoadSettings();
+    }
+
+    public void OnFullscreenToggle()
+    {
+        
+        gameSettings.fullscreen = Screen.fullScreen = fullscreenToggle.isOn;
+
+    }
+    public void OnResolutionChange()
+    {
+        Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
+    }
+    public void OnTextureQualityChange()
+    {
+        QualitySettings.masterTextureLimit = gameSettings.textureQuality = textureQualityDropdown.value;
+        
+    }
+    public void OnSoundVolumeChange()
+    {
+        soundSource.volume = gameSettings.soundVolume = soundVolumeSlider.value;
+    }
+    public void OnApplyButtonClick()
+    {
+        SaveSettings();
+    }
+    public void SaveSettings()
+    {
+        string jsonData = JsonUtility.ToJson(gameSettings, true);
+        File.WriteAllText(Application.persistentDataPath + "/gamesettings.json", jsonData);
+    }
+    public void LoadSettings()
+    {
+        gameSettings = JsonUtility.FromJson<GameSettings>(File.ReadAllText(Application.persistentDataPath + "/gamesettings.json"));
+        soundVolumeSlider.value = gameSettings.soundVolume;
+        textureQualityDropdown.value = gameSettings.textureQuality;
+        resolutionDropdown.value = gameSettings.resolutionIndex;
+        fullscreenToggle.isOn = gameSettings.fullscreen;
+    }
+}
